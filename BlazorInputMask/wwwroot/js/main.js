0e757a6164bb0b72efa24a3472813576eaeb73e7
Raphaël Désalbres <raphael.desalbres@outlook.com>
2021-06-13 10:20:38 +0200

    Added isRegEx parameter

--- BlazorInputMask/wwwroot/js/main.js
+++ BlazorInputMask/wwwroot/js/main.js
@@ -1,7 +1,12 @@
-﻿window.mask = (id,mask, dotnetHelper) => {
+﻿window.mask = (id, mask, isRegEx, dotnetHelper) => {
+    var pattern;    
+    if (isRegEx)
+        pattern = new RegExp(mask);
+    else
+        pattern = mask;    
     var customMask = IMask(
         document.getElementById(id), {
-            mask: mask,
+        mask: pattern,
             commit: function (value, masked) {
                 dotnetHelper.invokeMethodAsync('returnUnmaskedValue', this.unmaskedValue);                    
             }
