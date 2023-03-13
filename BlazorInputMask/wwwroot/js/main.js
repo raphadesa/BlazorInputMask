64312e1663b504238079d948914031ad7520c03a
Raphaël Désalbres <raphael.desalbres@outlook.com>
2023-03-13 18:55:22 +0100

    suppression //debugger

---
+++ BlazorInputMask/wwwroot/js/main.js
@@ -0,0 +1,26 @@
+﻿
+var customMask = null;
+
+<<<<<<< HEAD
+window.mask = (id, mask, isRegEx, destroy, dotnetHelper) => {    
+    var pattern;    
+=======
+window.mask = (id, mask, isRegEx, destroy, dotnetHelper) => {
+    var pattern;
+>>>>>>> f99bef71819ec2fbe41e5a913529a3b9cef8e08e
+    if (isRegEx)
+        pattern = new RegExp(mask);
+    else
+        pattern = mask;
+    if (customMask != null && destroy) {
+        customMask.destroy();
+    }
+    customMask = IMask(
+        document.getElementById(id), {
+        mask: pattern,
+        commit: function (value, masked) {
+            var vm = { value:value, unMaskedValue:this.unmaskedValue };
+            dotnetHelper.invokeMethodAsync('returnValue', vm);
+        }
+    });
+};
